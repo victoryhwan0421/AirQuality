7e36f5997028b00e734ef2e167d33c4d4a29bc5e
victoryhwan0421 <victoryhwan@naver.com>
2022-04-07 21:36:44 +0900

    add Swipe & Refresh, Loading, Exception handling

--- app/src/main/java/com/cookandroid/airquality/MainActivity.kt
+++ app/src/main/java/com/cookandroid/airquality/MainActivity.kt
@@ -5,6 +5,7 @@ import android.annotation.SuppressLint
 import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import androidx.core.app.ActivityCompat
 import com.cookandroid.airquality.data.Repository
 import com.cookandroid.airquality.data.models.airquality.Grade
@@ -32,6 +33,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
+        bindViews()
         initVariables()
         requestLocationPermissions()
 
@@ -65,6 +67,13 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    // 스와프하여 새로고침
+    private  fun bindViews() {
+        binding.refresh.setOnRefreshListener {
+            fetchAirQualityData()
+        }
+    }
+
 
     private fun initVariables(){
         fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
@@ -92,18 +101,33 @@ class MainActivity : AppCompatActivity() {
             cancellationTokenSource!!.token
         ).addOnSuccessListener { location ->
             scope.launch {
-                val monitoringStation =
-                    Repository.getNearbyMonitoringStation(location.latitude, location.longitude)
+                // 시작하고 errorDescription 발생 후 재시도했을 경우 에러화면을 제거해야하므로
+                binding.errorDescriptionTextView.visibility =View.GONE
+                try {
+                    val monitoringStation =
+                        Repository.getNearbyMonitoringStation(location.latitude, location.longitude)
 
-                // 관측소를 잘 불러오는지 확인!
-                // Optinal이므로 monitoringStation?. 로 safe_call 로 부르기
-                // binding.textView.text = monitoringStation?.stationName
+                    // 관측소를 잘 불러오는지 확인!
+                    // Optinal이므로 monitoringStation?. 로 safe_call 로 부르기
+                    // binding.textView.text = monitoringStation?.stationName
+                    val measuredValue =
+                        Repository.getLatestAirQualityData(monitoringStation!!.stationName!!)
 
-                val measuredValue =
-                    Repository.getLatestAirQualityData(monitoringStation!!.stationName!!)
+                    //binding.textView.text = measuredValue.toString()
+                    displayAirQualityData(monitoringStation, measuredValue!!)
+                } catch (exception: Exception) {
+                    // 문제가 발생했다면 errorDescriptionTextView 보이기
+                    binding.errorDescriptionTextView.visibility =View.VISIBLE
+                    // 정상적으로 로딩되었다가 재시도할 때 Exception 이 발생하여
+                    // errorDescriptionTextView 를 중복으로 보여주는 것을 방지하기 위해 alpha값을 0으로 설정
+                    binding.contentsLayout.alpha = 0F
 
-                //binding.textView.text = measuredValue.toString()
-                displayAirQualityData(monitoringStation, measuredValue!!)
+                } finally {
+                    // 모든 처리가 잘 이루어졌다면, 로딩창과, 새로고침 마무리
+                    binding.progressBar.visibility = View.GONE
+                    binding.refresh.isRefreshing = false
+                }
+
             }
            // binding.textView.text = "${location.latitude}, ${location.longitude}"
         }
@@ -111,6 +135,11 @@ class MainActivity : AppCompatActivity() {
 
     @SuppressLint("SetTextI18n")
     fun displayAirQualityData(monitoringStation: MonitoringStation, measuredValue: MeasuredValue) {
+        binding.contentsLayout.animate()
+            // alpha값을 default로 설정되어 있는 시간에 걸쳐 1로 변환(fade-in효과)
+            .alpha(1F)
+            .start()
+
         binding.measuringStationNameTextView.text = monitoringStation.stationName
         binding.measuringStationAddressTextView.text = monitoringStation.addr
 
